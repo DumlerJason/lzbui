d306ea49aacc0aec289be24ae19efe311141527d
DumlerJason <DrDoomler@gmail.com>
2025-07-13 13:35:19 -0400

    Little code cleanup

--- src/LzbUi.lua
+++ src/LzbUi.lua
@@ -1,10 +1,5 @@
 local _, AddOn = ...
---_G.AddOn = AddOn
-
-print("LzbUi Loading")
-
 AddOn.Nameplates:Init()
 AddOn.Resizer:Init()
-
 -- Print a message to indicate the addon has loaded
 print("LzbUi Loaded")
